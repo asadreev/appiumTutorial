22949aa84edcfbfa018a1f54e628324a2de893d5
asopin <alexey.sopin@gmail.com>
2019-11-21 14:03:41 +0200

    Changed to use POM and Page Factory

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,12 +3,36 @@ package pages;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
+
+
+    private AndroidDriver<AndroidElement> driver;
+
+    public MainPage() {
+    }
+
+    public MainPage(AndroidDriver<AndroidElement> driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Open\"]")
+    private AndroidElement openMenu;
+
+
+    public void clickonMenuOpenItem() {
+        openMenu.click();
+    }
+
+
+    public void openMenu() {
+        clickonMenuOpenItem();
+    }
 
 
 }
