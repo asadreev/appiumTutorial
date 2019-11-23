9ba4ab190d338a8701eb743ba738c50d75f0a010
asopin <alexey.sopin@gmail.com>
2019-11-23 14:30:35 +0200

    Changed IOS capabilities

--- src/test/java/IosTest.java
+++ src/test/java/IosTest.java
@@ -1,52 +1,49 @@
-public class IosTest {
-
-
-    import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.MobileCapabilityType;
-    public class AppTest {
-        public static URL url;
-        public static DesiredCapabilities capabilities;
-        public static IOSDriver<IOSElement> driver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-        //1
-        @BeforeSuite
-        public void setupAppium() throws MalformedURLException {
-            //2
-            final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
-            url = new URL(URL_STRING);
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-            //3
-            capabilities = new DesiredCapabilities();
-            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.4");
-            capabilities.setCapability(MobileCapabilityType.APP, "[PATH_TO_YOUR_.APP_FILE_THAT_YOU_COMPILED]");
-            capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
-            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-            capabilities.setCapability("useNewWDA", false);
-            //4
-            driver = new IOSDriver<IOSElement>(url, capabilities);
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            driver.resetApp();
-        }
-        //5
-        @AfterSuite
-        public void uninstallApp() throws InterruptedException {
-            driver.removeApp("com.bzytan.appiumeveryday");
-        }
-        //6
-        @Test (enabled=true) public void myFirstTest() throws InterruptedException {
-            driver.resetApp();
-        }
+public class IosTest {
+
+    public static URL url;
+    public static DesiredCapabilities capabilities;
+    public static IOSDriver<IOSElement> driver;
+
+    @BeforeSuite
+    public void setupAppium() throws MalformedURLException {
+
+        final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
+        url = new URL(URL_STRING);
+
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.2");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/admin/Desktop/testAppAppium/build/Release-iphonesimulator");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        capabilities.setCapability("useNewWDA", false);
+
+        driver = new IOSDriver<IOSElement>(url, capabilities);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.resetApp();
+    }
+
+    @AfterSuite
+    public void uninstallApp() throws InterruptedException {
+        driver.removeApp("com.app.testAppAppium");
+    }
+        
+
+    @Test(enabled = true)
+    public void myFirstTest() throws InterruptedException {
+        driver.resetApp();
+    }
 }
